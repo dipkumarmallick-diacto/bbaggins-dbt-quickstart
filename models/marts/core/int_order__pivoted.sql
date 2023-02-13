dc6047555828ddb3a13f3e30949001a52611fdda
Dip Kumar Mallick <dipkumar.mallick@diacto.com>
2023-02-13 05:12:01 +0000

    Changes

--- models/marts/core/int_order__pivoted.sql
+++ models/marts/core/int_order__pivoted.sql
@@ -1,7 +1,24 @@
-with
-    payments as (select * from {{ ref("stg_payments") }}),
+{%- set payment_methods = ['bank_transfer', 'credit_card', 'coupon', 'gift_card'] -%}
 
-    pivoted as (select * from payments)
+with payments as (
+   select * from {{ ref('stg_payments') }}
+),
 
-select *
-from pivoted
+final as (
+   select
+       order_id,
+       {% for payment_method in payment_methods -%}
+
+       sum(case when payment_method = '{{ payment_method }}' then amount else 0 end)
+            as {{ payment_method }}_amount
+
+       {%- if not loop.last -%}
+         ,
+       {% endif -%}
+
+       {%- endfor %}
+   from {{ ref('stg_payments') }}
+   group by 1
+)
+
+select * from final
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,12 +1,14 @@
-with payments as (
-    select
-        id as order_id,
-        user_id as customer_id,
-        paymentmethod as payment_method,
-        status,
-        amount / 100 as amount,
-        created as created_as
-    from {{source ('stripe','payment')}}
-)
+with
+    payments as (
+        select
+            id as order_id,
+            user_id as customer_id,
+            paymentmethod as payment_method,
+            status,
+            amount / 100 as amount,
+            created as created_as
+        from {{ source("stripe", "payment") }}
+    )
 
-select * from payments
+select *
+from payments
